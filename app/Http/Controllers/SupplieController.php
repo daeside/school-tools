84478e9890aff40a537b26e48f2eafbdba664ed5
daeside <daeside123@gmail.com>
2024-09-06 20:12:55 -0500

    Agrego mas modelos a usar, con las tablas de la base de datos

--- app/Http/Controllers/SupplieController.php
+++ app/Http/Controllers/SupplieController.php
@@ -3,15 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Models\Supplie;
+use App\Models\SupplieImage;
 use App\Models\User;
 
 class SupplieController extends Controller
 {
     public function index()
     {
-        $supplie = new Supplie();
+        $supplie = new User();
         $all = $supplie->all()[0];
-        var_dump($all);
+        var_dump($all->password);
         return null;
     }
 }
