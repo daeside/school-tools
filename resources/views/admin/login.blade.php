5f9076569158893d40e145c778b8cafbf7c29c9c
daeside <daeside123@gmail.com>
2024-09-22 14:55:04 -0500

    cambios en las formas que se muestran las vistas

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -1,35 +1,35 @@
-@include('partials._header')
+@extends('layouts.app')
+@section('content')
 <div class="container">
-<div class="row center">
-    <div class="col s3">
-    </div>
-    <div class="col s12 m6">
-        <div class="card grey lighten-5">
-            <form id="login" action="/admin/login" method="post">
-                <div class="card-content">
-                    <i class="large material-icons">account_circle</i>
-                    <div class="row">
-                        <div class="input-field">
-                            <i class="material-icons prefix">person</i>
-                            <input class="validate" type="text" name="user" id="user" required="" />
-                            <label for="user">Usuario</label>
+    <div class="row center">
+        <div class="col s3">
+        </div>
+        <div class="col s12 m6">
+            <div class="card grey lighten-5">
+                <form id="login" action="/admin/login" method="post">
+                    <div class="card-content">
+                        <i class="large material-icons">account_circle</i>
+                        <div class="row">
+                            <div class="input-field">
+                                <i class="material-icons prefix">person</i>
+                                <input class="validate" type="text" name="user" id="user" required="" />
+                                <label for="user">Usuario</label>
+                            </div>
+                            <div class="input-field">
+                                <i class="material-icons prefix">lock</i>
+                                <input class="validate" type="password" name="password" id="password" required="" />
+                                <label for="password">Contraseña</label>
+                            </div>
                         </div>
-                        <div class="input-field">
-                            <i class="material-icons prefix">lock</i>
-                            <input class="validate" type="password" name="password" id="password" required="" />
-                            <label for="password">Contraseña</label>
+                        <div class="card-action">
+                            <button class="waves-effect waves-light btn blue-grey darken-3" id="login-start">ingresar</button>
                         </div>
                     </div>
-                    <div class="card-action">
-                        <button class="waves-effect waves-light btn blue-grey darken-3" id="login-start">ingresar</button>
-                    </div>
-                </div>
-            </form>
+                </form>
+            </div>
+        </div>
+        <div class="col s3">
         </div>
     </div>
-    <div class="col s3">
-    </div>
 </div>
-</div>
-</body>
-</html>
+@endsection
